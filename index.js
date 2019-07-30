f2162870f53fffe2cf5255c7b8dacf64992678d9
atribecalledarty <arth3rs0ng@gmail.com>
2019-07-30 15:26:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if (location >= 42) {
+        return location - 42;
+    } else {
+        return 42 - location;
+    }
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(startingLocation, endingLocation) {
+    if (endingLocation >= startingLocation) {
+        return (endingLocation - startingLocation) * 264;
+    } else {
+        return (startingLocation - endingLocation) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let totalDistance = distanceTravelledInFeet(start, destination);
+
+    if (totalDistance < 400) {
+        return 0;
+    } else if (totalDistance > 2500) {
+        return 'cannot travel that far';
+    } else if (totalDistance < 2000) {
+
+        return (totalDistance - 400) * .02;
+    } else {
+        return 25;
+    }
+
+}
